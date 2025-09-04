fdd327644113477d9de361c12513157c4d3965bd
Albin Lindqvist <albin.lindqvist@hotmail.se>
2025-09-04 15:12:00 +0200

    put out cards

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import { Rank, Suit } from "./enums";
 function App() {
     return (
         <>
-            <Card open={false} rank={Rank.ace} suit={Suit.heart} />
-            <Card open={true} rank={Rank.five} suit={Suit.diamond} />
-            <Card open={true} rank={Rank.jack} suit={Suit.spade} />
+            <Card open={false} rank={Rank.ace} suit={Suit.heart} position="deck" index={0}/>
+            <Card open={false} rank={Rank.ace} suit={Suit.heart} position="deck" index={1}/>
+            <Card open={false} rank={Rank.ace} suit={Suit.heart} position="deck" index={2}/>
+            <Card open={false} rank={Rank.ace} suit={Suit.heart} position="deck" index={3}/>
+            <Card open={true} rank={Rank.jack} suit={Suit.spade} position="player" index={0}/>
+            <Card open={true} rank={Rank.five} suit={Suit.diamond} position="player" index={1}/>
+            <Card open={true} rank={Rank.two} suit={Suit.club} position="dealer" index={0}/>
+            <Card open={false} rank={Rank.two} suit={Suit.club} position="dealer" index={1}/>
         </>
     );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,16 +4,26 @@ interface CardProps {
     open: boolean;
     rank: Rank;
     suit: Suit;
+    position: "dealer" | "deck" | "player";
+    index: number;
 }
 
-export default function Card({ open, rank, suit }: CardProps) {
+export default function Card({ open, rank, suit, index, position }: CardProps) {
     let isBlackSuit = false;
     if (suit === Suit.club || suit === Suit.spade) {
         isBlackSuit = true;
     }
+    const offset = position === "deck" ? 4 : 110
+    const stackingOffset = index * offset;
+
+    const positions = {
+        deck: { top: 20 + stackingOffset, right: 100 + stackingOffset},
+        dealer: { top: 100, left: 200 + stackingOffset },
+        player: { top: 350, left: 200 + stackingOffset },
+    };
 
     return (
-        <div className={"card" + (open ? "" : " closed")}>
+        <div className={"card" + (open ? "" : " closed")} style={positions[position]}>
             {open ? (
                 <div className={isBlackSuit ? "black" : "red"}>
                     <p className="rank">{rank}</p>
